5da264183d32653f8a14e21fb6331ee011813247
ReCursia <saha2098@yandex.ru>
2020-04-13 03:38:47 +0300

    implementing data layer progress

---
+++ FestivalWebApp.Data/Repositores/FestivalRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using FestivalWebApp.Domain.Models;
+using FestivalWebApp.Domain.Repositories;
+
+namespace FestivalWebApp.Data.Repositores
+{
+    public class FestivalRepository : IFestivalRepository
+    {
+        public Festival GetFestivalById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Festival> GetAllFestivals()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddFestival(Festival festival)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateFestival(Festival festival)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveFestival(Festival festival)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FestivalWebApp.Data/Repositores/ParticipantRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using FestivalWebApp.Domain.Models;
+using FestivalWebApp.Domain.Repositories;
+
+namespace FestivalWebApp.Data.Repositores
+{
+    public class ParticipantRepository : IParticipantRepository
+    {
+        public Participant GetParticipantById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Participant> GetAllParticipants()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Participant> GetParticipantsByFestivalId(int festivalId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddParticipant(Participant participant)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateParticipant(Participant participant)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveParticipant(Participant participant)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
