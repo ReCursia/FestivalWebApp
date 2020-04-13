5836f86c02044280ff34ce5576b9f03f17205c59
ReCursia <saha2098@yandex.ru>
2020-04-13 03:17:36 +0300

    implementing domain progress

--- FestivalWebApp.Domain/Models/Participant.cs
+++ FestivalWebApp.Domain/Models/Participant.cs
@@ -2,6 +2,7 @@
 {
     public class Participant
     {
+        public int Id { get; set; }
         public int Age { get; set; }
         public int FestivalId { get; set; }
         public string Name { get; set; }
