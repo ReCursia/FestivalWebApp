a216cb7390f066a3a8f1d06acca40a76bd169411
ReCursia <saha2098@yandex.ru>
2020-04-20 01:45:51 +0300

    implementing progress...

---
+++ FestivalWebApp.API/Validators/FestivalCreateValidator.cs
@@ -0,0 +1,17 @@
+﻿using FestivalWebApp.API.Models;
+using FluentValidation;
+
+namespace FestivalWebApp.API.Validators
+{
+    public class FestivalCreateValidator : AbstractValidator<FestivalCreateRequestBody>
+    {
+        private const int FestivalNameMaxSize = 50;
+        private const int FestivalDescriptionMaxSize = 200;
+
+        public FestivalCreateValidator()
+        {
+            RuleFor(f => f.Name).MaximumLength(FestivalNameMaxSize);
+            RuleFor(f => f.Description).MaximumLength(FestivalDescriptionMaxSize);
+        }
+    }
+}
---
+++ FestivalWebApp.API/Validators/ParticipantUpdateValidator.cs
@@ -0,0 +1,19 @@
+﻿using FestivalWebApp.API.Models;
+using FluentValidation;
+
+namespace FestivalWebApp.API.Validators
+{
+    public class ParticipantUpdateValidator : AbstractValidator<ParticipantUpdateRequestBody>
+    {
+        private const int NameMaxSize = 50;
+        private const int SecondNameMaxSize = 50;
+        private const string ErrorMessage = "Id must be greater than 0.";
+
+        public ParticipantUpdateValidator()
+        {
+            RuleFor(p => p.FestivalId).GreaterThan(0).WithMessage(ErrorMessage);
+            RuleFor(p => p.Name).MaximumLength(NameMaxSize);
+            RuleFor(p => p.SecondName).MaximumLength(SecondNameMaxSize);
+        }
+    }
+}
